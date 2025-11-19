9f5ae086115146a3bdded54a43d0419a968426d8
Madison <madisonhauptmann@gmail.com>
2025-11-19 10:55:18 -0500

    created first api route to get songs mh

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 // setup
 const express = require('express');
-const app = express();
-const router = express.Router();
+const cors = require('cors');
 
-// start the webserver
-app.listen(3000, function(){
-    console.log("Listening on port 3000");
-});
+const app = express();
+app.use(cors());
+const router = express.Router();
 
 // making an api using routes
 
-// Get request - /hello
+router.get("/songs", function(req, res){
+    const songs = [
+        {
+            title: "Uptown Funk",
+            artist: "Bruno Mars",
+            popularity: 10,
+            releaseDate: new Date("2011, 9, 22"),
+            genre: ["Funk", "Boogie"]
+        },
+        {
+            title: "We Found Love",
+            artist: "Rhianna",
+            popularity: 10,
+            releaseDate: new Date("2013, 11, 21"),
+            genre: ["Electro House"]
+        },
+        {
+            title: "Happy",
+            artist: "Pharrell Williams",
+            popularity: 10,
+            releaseDate: new Date("2013, 11, 21"),
+            genre: ["Soul", "Pop"]
+        }
+    ];
+
+    res.json(songs);
+});
+
+app.use("/api", router);
+app.listen(3000);
