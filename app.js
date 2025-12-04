fa5fafb735d22cdd246fbd2dff47c5762e5661ea
Madison <madisonhauptmann@gmail.com>
2025-12-04 13:57:00 -0500

    add more routes mh

--- app.js
+++ app.js
@@ -20,13 +20,38 @@ router.get("/songs", async function (req, res) {
     }
 });
 
+// grab a single song
+router.get("/songs/:id", async(req, res) => {
+    try{
+        const song = await Song.findById(req.params.id);
+        res.json(song);
+    }
+    catch (err){
+        res.status(400).send(err);
+    }
+});
 
+
+// add a song to the database
 router.post("/songs", async(req, res) => {
     try{
         const song = await new Song(req.body);
         await song.save();
         res.status(201).json(song);
         console.log(song);
+    }
+    catch(err) {
+        res.status(400).send(err);
+    }
+})
+
+// update an exsisting song
+router.put("/songs/:id", async(req, res) => {
+    try{
+        const song = req.body;
+        await Song.updateOne({_id : req.params.id}, song);
+        console.log(song);
+        res.sendStatus(204);
     }
     catch(err) {
         res.status(400).send(err);
--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
 mongoose.connect("mongodb+srv://sdev265:Password@songdb.qvlebxh.mongodb.net/?appName=SongDB");
 
+console.log("MongoDB connected")
 module.exports = mongoose;
